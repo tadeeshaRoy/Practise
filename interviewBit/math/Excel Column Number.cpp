25ad95bcb860fc5c5d4d72c203ea3bca4935cb2a
Tadeesha <35450690+tadeeshaRoy@users.noreply.github.com>
2019-01-06 02:08:17 +0530

    Create Excel Column Number.cpp

---
+++ interviewBit/math/Excel Column Number.cpp
@@ -0,0 +1,41 @@
+/*
+Given a column title as appears in an Excel sheet, return its corresponding column number.
+
+Example:
+
+    A -> 1
+    
+    B -> 2
+    
+    C -> 3
+    
+    ...
+    
+    Z -> 26
+    
+    AA -> 27
+    
+    AB -> 28 
+*/
+
+int Solution::titleToNumber(string A) {
+    //char c = 'A';
+    //cout<<int(c);
+    
+    int n = A.length(), p = n - 1, col_num = 0; char c;
+    
+    for(int i=0;i<n;i++)
+    {
+        c = A[i] - 64;
+        col_num += pow(26,p)*c;
+        p--;
+    }
+    
+    return col_num;
+}
+
+// http://www.ascii-code.net/
+
+// https://www.programiz.com/cpp-programming/examples/ASCII-value-character
+
+// https://spreadsheeto.com/columns-numbers/
