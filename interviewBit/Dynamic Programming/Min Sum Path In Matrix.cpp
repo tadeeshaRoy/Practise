f1a8447f6d82b371878581dd7e7ac21e736bbe51
Tadeesha <35450690+tadeeshaRoy@users.noreply.github.com>
2019-04-10 00:42:50 +0530

    Update Min Sum Path In Matrix.cpp

--- interviewBit/Dynamic Programming/Min Sum Path In Matrix.cpp
+++ interviewBit/Dynamic Programming/Min Sum Path In Matrix.cpp
@@ -26,12 +26,14 @@ How to define 2D vector:
 
 */
 
+/*
+In case diagonal is allowed as well
 int min(int x,int y, int z)
 {
     if(x<y)
         return (x<z)?x:z;
     else return (y<z)?y:z;
-}
+}*/
 
 int Solution::minPathSum(vector<vector<int> > &A) {
     
@@ -57,3 +59,40 @@ int Solution::minPathSum(vector<vector<int> > &A) {
     
     return temp[m-1][n-1];
 }
+
+
+//interviewBit solution
+
+int Solution::minPathSum(vector<vector<int> > &A) {
+       if(A.size() == 0){
+        return 0;
+    }
+    
+    int rows = A.size();
+    int cols = A[0].size();
+    
+    vector<vector<int> > temp(rows, vector<int>(cols));
+    
+    int sum = 0;
+    
+    for(int i = 0; i < cols; i++){
+        temp[0][i] = sum + A[0][i];
+        sum = temp[0][i];
+    }
+    
+    sum = 0;
+    
+    for(int i = 0; i < rows; i++){
+        temp[i][0] = sum + A[i][0];
+        sum = temp[i][0];
+    }
+    
+    for(int i = 1; i < rows; i++){
+        for(int j = 1; j < cols; j++){
+            temp[i][j] = A[i][j] + min(temp[i-1][j], temp[i][j-1]);
+        }
+    }
+    
+    return temp[rows-1][cols-1];
+}
+
